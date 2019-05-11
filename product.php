9d99b6e41c993c9c559f636ffba765aedc54f404
Rowen Borgonia <rrborgonia@gmail.com>
2019-05-11 14:22:00 +0800

    second commit

--- product.php
+++ product.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	if(isset($_SESSION['txtUsername'])){
-		echo "<h2>Welcome to Product Page </h2>";
+		echo "<h2>Welcome to Products Page </h2>";
 		echo "<br><a href='welcome.php'><input type ='button' name='btnBack' value='Back'>";
 	}else{
 		echo "<script>alert('You must log-in first before proceed!!!')</script>";
